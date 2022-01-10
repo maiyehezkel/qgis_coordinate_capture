66b29c130a5683db2953f2ed117ba52807745a5e
Mai Yehezkel <57328865+maiyehezkel@users.noreply.github.com>
2022-01-10 15:03:17 +0200

    Update coordinate_capture.py

--- coordinate_capture.py
+++ coordinate_capture.py
@@ -25,7 +25,7 @@ from qgis.PyQt.QtCore import QSettings, QTranslator, QCoreApplication, Qt
 from qgis.PyQt.QtGui import QIcon
 from qgis.PyQt.QtWidgets import QAction
 from qgis.gui import QgsProjectionSelectionDialog
-from qgis.core import QgsUnitTypes, QgsCoordinateTransform, QgsCoordinateReferenceSystem, QgsPointXY
+from qgis.core import QgsUnitTypes, QgsCoordinateTransform, QgsCoordinateReferenceSystem, QgsPointXY, QgsApplication
 # Initialize Qt resources from file resources.py
 from .resources import *
 
