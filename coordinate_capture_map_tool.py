e40c6de48e15058c1a5043d09e73134dfdbf7827
Mai Yehezkel <57328865+maiyehezkel@users.noreply.github.com>
2022-01-10 13:49:03 +0200

    Update coordinate_capture_map_tool.py

--- coordinate_capture_map_tool.py
+++ coordinate_capture_map_tool.py
@@ -37,7 +37,7 @@ class CoordinateCaptureMapTool(QgsMapToolEmitPoint):
         self.rubberBand = QgsRubberBand(self.mapCanvas, QgsWkbTypes.PolygonGeometry)
         self.rubberBand.setColor(Qt.red)
         self.rubberBand.setWidth(3)
-        if CoordinateCaptureDockWidget.dockwidget.crossMouseButton.isChecked():
+        if CoordinateCaptureDockWidget.crossMouseButton.isChecked():
             self.setCursor(QgsApplication.getThemeCursor(QgsApplication.Cursor.CrossHair))
         else:
             self.setCursor(QgsApplication.getThemeCursor(QgsApplication.Cursor.CapturePoint))
